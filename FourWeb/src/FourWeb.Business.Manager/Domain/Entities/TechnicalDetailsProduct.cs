c0ffca38d1c7d9bf78247cf919b9c6dbf93ee98d
cassianomendes <cassianomendes.info@gmail.com>
2016-09-04 19:27:51 -0300

    First entities

---
+++ FourWeb/src/FourWeb.Business.Manager/Domain/Entities/TechnicalDetailsProduct.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FourWeb.Business.Manager.Domain.Entities
+{
+    public class TechnicalDetailsProduct
+    {
+    }
+}
